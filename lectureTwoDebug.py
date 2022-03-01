eb6ebb31f9ffb6593249587912098a9d7ecab318
dandocmando <danferguson2002@gmail.com>
2022-03-01 15:28:45 +1100

    comments created

---
+++ lectureTwoDebug.py
@@ -0,0 +1,13 @@
+def main():
+    name = input("What is your name?\n")
+    birthday = input("What is your birthday?\n")
+    capacity = input("What capacity are you operating in?\n")
+    likes = input("What do you like today?\n")
+
+    print("My name is " + name + ".")
+    print("I was born on " + birthday + ".")
+    print("I am here in the capacity of " + capacity + ".")
+    print("I really like " + likes + ".")
+
+
+main()
