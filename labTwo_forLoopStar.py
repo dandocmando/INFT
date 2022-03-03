6b318656f0bff3a1691bfe69a0add5e9d465757f
dandocmando <danferguson2002@gmail.com>
2022-03-03 15:57:15 +1100

    Lab Two

--- labTwo_forLoopStar.py
+++ labTwo_forLoopStar.py
@@ -8,9 +8,13 @@ def genAsterLines(numOfLines):
 
     line = int(input("Enter an integer (0~10): \n"))
     ast = "*"
+    blank = " "
 
     for i in range(line):
-        print(ast*i)
+       # print(blank*i,ast)
+
+
+        print(blank*i,ast*i)
         if i == line-1:
 
             print(ast*i+ast)
