0465a37bcfe2ca3299bd15b385d6a61cbbf52505
dandocmando <danferguson2002@gmail.com>
2022-03-08 14:41:34 +1100

    updated naming

--- labTwo_forloopV3.py
+++ labTwo_forloopV3.py
@@ -5,19 +5,20 @@ Date: 8/3/22
 Creates a diamond.
 """
 
-def genAsterLines():
+
+def diamondgenerator():
     line = int(input("Enter an integer: \n"))
     ast = "*"
     blank = " "
 
     for i in range(line):
 
-        print(blank*(line-i)+ast+ast*i+ast*i)
-        if i == line-1:
-            print((blank*(line-(i+1))+ast+ast*(i+1)+ast*(i+1)))
+        print(blank * (line - i) + ast + ast * i + ast * i)
+        if i == line - 1:
+            print((blank * (line - (i + 1)) + ast + ast * (i + 1) + ast * (i + 1)))
 
             for x in range(line)[::-1]:
                 print(blank * (line - x) + ast + ast * x + ast * x)
 
 
-genAsterLines()
+diamondgenerator()
