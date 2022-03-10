12422fc3ea690dee20a238ffa59300ecceefb286
dandocmando <danferguson2002@gmail.com>
2022-03-10 23:57:58 +1100

    added more two rock

--- RockPaperScissorsV2.py
+++ RockPaperScissorsV2.py
@@ -1,7 +1,7 @@
 """
 Boring stuff
 Author: Dan
-Date: 3/3/22
+Date: 10/3/22
 """
 
 import random
@@ -15,19 +15,47 @@ class rockPaperScissors():
         rockPaperScissors.playerName = playerName
         rockPaperScissors.outcomes = outcomes
 
+    def introduction(self):
+        print("This is a game of rock paper scissors!")
+        #time.sleep(0.5)
+        print("You will say rock, paper or scissors and the computer will play against you.")
+        #time.sleep(0.5)
+        print("The inputs are: R = Rock, S = Scissors, P = paper.")
+        print("You get five rounds to win.\n")
+        #time.sleep(5)
+
 
 class computer():
     def __init__(self):
         self.outcomes = rockPaperScissors.outcomes
-        self.player = rockPaperScissors.playerName
+        #self.player = rockPaperScissors.playerName
+
 
     def compChoice(self):
-        print(self.outcomes)
+        # print(self.outcomes)
         compChoice = random.choice(self.outcomes)
         print(compChoice)
 
+class player():
+    def __init__(self):
+        self.player = rockPaperScissors.playerName
+        self.playerScore = rockPaperScissors.playerScore
+
+    def playerChoice(self):
+        print("Guess either: R, P, S")
+        plChoice = input()
+
+
 
 playerName = "testinput"
-initialise_rockPaperScissors = rockPaperScissors(0, 0, playerName)
-compObject = computer()
-compObject.compChoice()
+
+# class initialisation and execution
+init_rockPaperScissors = rockPaperScissors(0, 0, playerName)  # initialises the rockpaperscissors class inputs name
+init_rockPaperScissors.introduction()  # executes the introduction def in rock++etc class
+
+compObject = computer()  # initialises the computer class as compObject
+compObject.compChoice()  # executes the compChoice def in computer class
+
+initPlayer = player()
+initPlayer.playerChoice()
+
