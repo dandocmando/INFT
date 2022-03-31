a77d1afc961f47ad4870c1c2465ec5cf7cd4b1e3
dandocmando <danferguson2002@gmail.com>
2022-03-31 13:38:08 +1100

    completed rockpaperscissors game.

--- RockPaperScissorsV4_singleClass.py
+++ RockPaperScissorsV4_singleClass.py
@@ -1,7 +1,8 @@
 """
 Boring stuff
 Author: Dan
-Date: 10/3/22
+Date: 31/3/22
+Functionality complete
 """
 
 import random
@@ -10,6 +11,7 @@ import time
 
 class RockPaperScissors:
     def __init__(self, playerName):
+        self.player_choice = None
         self.playerScore = 0
         self.compScore = 0
         self.playerName = playerName
@@ -24,34 +26,67 @@ class RockPaperScissors:
         print("You get five rounds to win.\n")
         # time.sleep(5)
 
-    def comp_vs_user(self):
-        print("nnn")
-
     def comp_choice(self):
-
-        # print(random.choice(list(self.outcomes)))
         return random.choice(list(self.outcomes))
 
     def user_choice(self):
         enter_legal_choice = False
         while not enter_legal_choice:
             print("Guess either: R, P, S")
-            player_choice = input()
+            self.player_choice = input()
 
-            if player_choice not in self.outcomes:
+            if self.player_choice not in self.outcomes:
                 print("You didn't input R, P or S. Please try again.")
 
             else:
                 enter_legal_choice = True
 
+    def main_game(self):
+
+        for i in range(5):
             comp_choice = self.comp_choice()
-            print("test")
+            #print("comp: " + comp_choice)  # used for debugging
+            self.user_choice()
+            if comp_choice == self.player_choice:
+                print("Its a draw! Both you and the computer guessed the same answer.")
 
+            elif self.player_choice == "R" and comp_choice == "P":
+                print("Computer wins! You chose: " + self.player_choice + " computer chose: " + comp_choice)
+                self.compScore += 1
 
-# playerName = "testinput"
+            elif self.player_choice == "R" and comp_choice == "S":
+                print("You win! You chose: " + self.player_choice + " computer chose: " + comp_choice)
+                self.playerScore += 1
+
+            elif self.player_choice == "P" and comp_choice == "R":
+                print("You win! You chose: " + self.player_choice + " computer chose: " + comp_choice)
+                self.playerScore += 1
+
+            elif self.player_choice == "P" and comp_choice == "S":
+                print("Computer wins! You chose: " + self.player_choice + " computer chose: " + comp_choice)
+
+            elif self.player_choice == "S" and comp_choice == "R":
+                print("Computer wins! You chose: " + self.player_choice + " computer chose: " + comp_choice)
+                self.compScore += 1
+
+            elif self.player_choice == "S" and comp_choice == "P":
+                print("You win! You chose: " + self.player_choice + " computer chose: " + comp_choice)
+                self.playerScore += 1
+
+        if self.playerScore > self.compScore:
+            print(str(self.playerName)+" wins by "+str(self.playerScore-self.compScore)+" points!")
+
+        elif self.compScore > self.playerScore:
+            print("Computer wins by"+str(self.compScore-self.playerScore)+" points!")
+
+        elif self.playerScore == self.compScore:
+            print("Its a tie!")
+
+        else:
+            print("Nobody won go away loser!")
+
 
 # class initialisation and execution
 init_rockPaperScissors = RockPaperScissors("Dan")  # initialises the rockpaperscissors class inputs name
 init_rockPaperScissors.introduction()  # executes the introduction def in rock++etc class
-init_rockPaperScissors.comp_choice()
-init_rockPaperScissors.user_choice()
+init_rockPaperScissors.main_game()  # starts the main game
